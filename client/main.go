e147886ffe6b35a1076c726a69428e8f36d99950
artyom <artyom.sushkov@gmail.com>
2021-07-26 13:01:35 +0300

    add is working

--- client/main.go
+++ client/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"example.com/grpcapp/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":8000", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Went wrong 1")
+		return
+	}
+	c := proto.NewAppClient(conn)
+	res, err := c.Add(context.Background(), &proto.AddValues{X: 3, Y: 4})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res.GetX())
+}
--- server/main.go
+++ server/main.go
@@ -1,4 +1,64 @@
 package main
 
+import (
+	"context"
+	"io"
+	"net"
+
+	"example.com/grpcapp/proto"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type GRPCServer struct {
+	proto.UnimplementedAppServer
+}
+
+func (s *GRPCServer) Add(ctx context.Context, req *proto.AddValues) (*proto.Value, error) {
+	return &proto.Value{X: req.GetX() + req.GetY()}, nil
+}
+
+func (s *GRPCServer) Sum(stream proto.App_SumServer) error {
+	var res int64
+	for {
+		x, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&proto.Value{
+				X: res,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		res += x.GetX()
+	}
+}
+
+func (s *GRPCServer) Ones(x *proto.Value, stream proto.App_OnesServer) error {
+	for it := x.GetX(); it >= 0; it -= 1 {
+		if err := stream.Send(&proto.Value{
+			X: 1}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *GRPCServer) Repeat(stream proto.App_RepeatServer) error {
+	return status.Errorf(codes.Unimplemented, "method Repeat not implemented")
+}
+
 func main() {
+	s := grpc.NewServer()
+	srv := &GRPCServer{}
+	proto.RegisterAppServer(s, srv)
+	l, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		return
+	}
+	if err := s.Serve(l); err != nil {
+		return
+	}
 }
